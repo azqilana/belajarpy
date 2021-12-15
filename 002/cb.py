959a83cb6ae1cb2a2d49c155cd1c71b117275b36
azqilana <mazqulunu@gmail.com>
2021-12-15 10:33:28 +0800

    tambah materi variabel

---
+++ 002/cb.py
@@ -0,0 +1,25 @@
+#variabel dan type data
+#variabel tempat menyimpan data
+
+a = 1
+b = 2
+azqilana = 8
+
+print("Hasil dari penjumlahan variabel di atas adalah ", azqilana + b + a)
+
+#note penammaan
+# -membuat variabel tidak boleh ada sepasi ya spasi di gantikan dengan undescore / garis bawah
+nilai_nama = 4 
+print(nilai_nama)
+# -membuat variabel dapat dengan angka tapi harus di dahului huruf
+lima_puluh4 = 54 
+print(lima_puluh4)
+#- boleh mencampur huruf kecil dan besar
+niai_Saya8 = 8 
+print(niai_Saya8)
+
+
+
+
+
+
